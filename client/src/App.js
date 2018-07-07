f3561bf1a7351cc45367ccdc5628f0ec7fd7be2b
Medhir Bhargava <medhirbhargava@gmail.com>
2018-07-06 23:22:09 -0700

    YASSSSSSSSSSSSCSS fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Fragment, Component } from 'react';
 import Header from './Layout/Header';
 import Main from './Layout/Main';
 import Footer from './Layout/Footer';
-import './Reset.css';
 import './App.css';
 
 class App extends Component {
