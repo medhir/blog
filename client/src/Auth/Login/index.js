ea79b68ef4ff21dae7a66f012fb27864a678de99
Medhir Bhargava <medhirbhargava@gmail.com>
2019-03-22 16:29:09 -0700

    Revert "convert to typescript"
    This reverts commit 548daa9a3891880f6e4a351a8a05feb402a71e65.

---
+++ client/src/Auth/Login/index.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+
+class Auth extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render () {
+        return (
+            <section className="login">
+
+            </section>
+        )
+    }
+}
+
+export default Auth;
