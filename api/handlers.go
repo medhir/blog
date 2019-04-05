451cc10882e430dcc52fc7145fa9fe5f514da518
Medhir Bhargava <medhirbhargava@gmail.com>
2019-04-05 12:58:04 -0700

    Route photo uploads to april, auth requests to deployed URL

--- api/handlers.go
+++ api/handlers.go
@@ -110,7 +110,7 @@ func UploadPhoto() http.HandlerFunc {
 				return
 			}
 			processedImage := reduceFileSizeAndConvertToJPG(fileBuffer.Bytes())
-			result, err := putObject(bytes.NewReader(processedImage), "albums/march.2019/"+id+".jpg")
+			result, err := putObject(bytes.NewReader(processedImage), "albums/april/"+id+".jpg")
 			if err != nil {
 				fmt.Println(err.Error())
 				http.Error(w, "Could not upload file "+id+".jpg", http.StatusInternalServerError)
