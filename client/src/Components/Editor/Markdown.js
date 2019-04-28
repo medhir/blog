4ff590ea0983ececb9d63b61f706ac3cc0abb5c2
Medhir Bhargava <medhirbhargava@gmail.com>
2019-04-28 15:01:28 -0700

    Start work on photo upload in blog posts

--- client/src/Components/Editor/Markdown.js
+++ client/src/Components/Editor/Markdown.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
+const IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
+
 const Markdown = (props) => {
-    
+    const containsImage = (dtItems) => {
+        let containsImage = false;
+        for (let i = 0; i < dtItems.length; i++) {
+            if (IMAGE_MIME_REGEX.test(dtItems[i].type)) {
+                containsImage = true;
+                break;
+            }
+        }
+        return containsImage;
+    }
     const handleDrop = (e) => {
+        debugger;
         e.preventDefault();
         const files = e.dataTransfer.files;
         for (let i = 0; i < files.length; i++) {
@@ -16,7 +28,31 @@ const Markdown = (props) => {
     }
 
     const handlePaste = (e) => {
-        // e.preventDefault();
+        const items = e.clipboardData.items;
+        let blob;
+        if (containsImage(items)) {
+            e.preventDefault()
+            for (let i = 0; i < items.length; i++) {
+                if (IMAGE_MIME_REGEX.test(items[i].type)) {
+                    blob = items[i].getAsFile();
+                    break;
+                }
+            }
+            // upload file
+            const data = new FormData()
+            data.append('file', blob)
+            fetch('/api/blog/asset', {
+                method: 'POST', 
+                body: data
+            })
+        }
+        // get current textarea value
+        const value = e.target.value;
+        // store selection start/end positions
+        const start = e.target.selectionStart;
+        const end = e.target.selectionEnd;
+
+        debugger;
         console.log("A paste happened");
     }
 
