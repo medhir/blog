80d76bfd1fb95f04f16fe36b3f33c35c8e6ef7d8
Medhir Bhargava <medhirbhargava@gmail.com>
2019-04-28 17:52:10 -0700

    Add client logic to upload photo on paste / drop

--- client/src/Components/Editor/Markdown.js
+++ client/src/Components/Editor/Markdown.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { AuthUtil } from '../../Auth/AuthUtility'
+import http from '../../Utils/http'
 import TextareaAutosize from 'react-textarea-autosize';
 
 const IMAGE_MIME_REGEX = /^image\/(p?jpeg|gif|png)$/i;
 
-const Markdown = (props) => {
-    const containsImage = (dtItems) => {
+class Markdown extends Component {
+    constructor (props) {
+        super(props)
+    }
+
+    containsImage (dtItems) {
         let containsImage = false;
         for (let i = 0; i < dtItems.length; i++) {
             if (IMAGE_MIME_REGEX.test(dtItems[i].type)) {
@@ -14,23 +20,33 @@ const Markdown = (props) => {
         }
         return containsImage;
     }
-    const handleDrop = (e) => {
-        debugger;
-        e.preventDefault();
-        const files = e.dataTransfer.files;
-        for (let i = 0; i < files.length; i++) {
-            console.log(`
-            File Name: ${files[i].name}
-            File Type: ${files[i].type}
-            File Size: ${files[i].size}
-            `)
-        }
+
+    insertAtCursor (start, end, textToInsert, input) {
+        // get current text of the input
+        const value = input.value;
+        // update the value with new text
+        this.props.updateMarkdown(value.slice(0, start) + textToInsert + value.slice(end), () => {
+            input.selectionStart = input.selectionEnd = start + textToInsert.length;
+        })
     }
 
-    const handlePaste = (e) => {
+    handleDrop (e) {
+        const items = e.dataTransfer.items;
+        this.handleImageItems(e, items)
+    }
+
+    handlePaste (e) {
         const items = e.clipboardData.items;
+        this.handleImageItems(e, items)
+    }
+
+    handleImageItems (e, items) {
+        // store selection start/end positions
+        const start = e.target.selectionStart;
+        const end = e.target.selectionEnd;
+
         let blob;
-        if (containsImage(items)) {
+        if (this.containsImage(items)) {
             e.preventDefault()
             for (let i = 0; i < items.length; i++) {
                 if (IMAGE_MIME_REGEX.test(items[i].type)) {
@@ -38,32 +54,34 @@ const Markdown = (props) => {
                     break;
                 }
             }
+            // Set uploading message in textarea
+            this.insertAtCursor(start, end, '![](Uploading...)', e.target)
             // upload file
             const data = new FormData()
             data.append('file', blob)
-            fetch('/api/blog/asset', {
-                method: 'POST', 
-                body: data
+            http.post(`/api/blog/assets/${ this.props.id }`, data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `${ AuthUtil.token }`
+                }
+            }).then(response => {
+                console.log(response)
+            }).catch(err => {
+                console.error(err)
             })
         }
-        // get current textarea value
-        const value = e.target.value;
-        // store selection start/end positions
-        const start = e.target.selectionStart;
-        const end = e.target.selectionEnd;
-
-        debugger;
-        console.log("A paste happened");
     }
 
-    return (
-        <TextareaAutosize
-            value={ props.markdown } 
-            onChange={ props.parse } 
-            onDrop={ handleDrop }
-            onPaste={ handlePaste }
-        />
-    )
+    render () {
+        return (
+            <TextareaAutosize
+                value={ this.props.markdown } 
+                onChange={ this.props.parse } 
+                onDrop={ this.handleDrop.bind(this) }
+                onPaste={ this.handlePaste.bind(this) }
+            />
+        )
+    }
 }
 
 export default Markdown;
