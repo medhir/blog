e9c1f4cee39b46849eeb95cd41398d13592f4b0f
Medhir Bhargava <medhirbhargava@gmail.com>
2019-05-11 19:36:58 -0400

    Improve image compression logic

--- api/image.go
+++ api/image.go
@@ -7,21 +7,33 @@ import (
 )
 
 var reduceQuality = bimg.Options{
-	Quality:       30,
+	Quality:       85,
 	NoAutoRotate:  false,
-	StripMetadata: true}
+	StripMetadata: true,
+	Interlace:     true}
 
 func reduceFileSizeAndConvertToJPG(buffer []byte) []byte {
 	image := bimg.NewImage(buffer)
-	_, err := image.Process(reduceQuality)
-	if err != nil {
-		fmt.Println("Could not reduce image quality - ", err.Error())
-	}
-	metadata, _ := image.Metadata()
-	fmt.Println(metadata)
-	finalJpg, err := image.Convert(bimg.JPEG)
+	_, err := image.Convert(bimg.JPEG)
 	if err != nil {
 		fmt.Println("Could not convert to JPEG - ", err.Error())
 	}
-	return finalJpg
+	size, err := image.Size()
+	if err != nil {
+		fmt.Println("Could not get image size - ", err.Error())
+	}
+	if size.Width > 1000 {
+		scale := float64(size.Width) / float64(size.Height)
+		newWidth := 1000
+		newHeight := int(1000 / scale)
+		_, err = image.Resize(newWidth, newHeight)
+		if err != nil {
+			fmt.Println("Could not resize image - ", err.Error())
+		}
+	}
+	_, err = image.Process(reduceQuality)
+	if err != nil {
+		fmt.Println("Could not reduce image quality - ", err.Error())
+	}
+	return image.Image()
 }
