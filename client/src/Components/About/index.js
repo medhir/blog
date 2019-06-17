a789c76e30979245853dadb0d104a600230edb41
Medhir Bhargava <medhirbhargava@gmail.com>
2019-06-16 21:09:29 -0500

    Update About page (w/ new job ✨)

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -6,27 +6,25 @@ class About extends Component {
     return (
       <section className="about">
         <p>
-          Hello, you're visiting Medhir Bhargava's web page. I am a
-          Seattle-based software engineer and (sometimes) visual artist.
+          Hello, and welcome to my small internet outpost. I am a Seattle-based
+          software engineer currently working at{' '}
+          <a href="https://lightstep.com">LightStep</a>.
+        </p>
+        <p>
+          I sometimes like to dabble in visual artistry through photography and
+          3D modeling / rendering using{' '}
+          <a href="https://blender.org">Blender</a>.
         </p>
         <p>
           My interests span many domains, including web application development,
           biology, graphic design, generative art, and computer science
           education.
         </p>
-        {/* <p>My current choice of tools includes React and Go for web pplication development, and Blender for creative endeavors.</p> */}
         <p>
           You can view some of my work through this site and{' '}
           <a href="https://github.com/medhir">Github</a>, connect with me on{' '}
           <a href="https://linkedin.com/in/medhir">LinkedIn</a>, or send me an
           e-mail via mail AT medhir.com.
-        </p>
-        <p>
-          Currently available for hire for Seattle-based or remote work. Please{' '}
-          <a href="https://s3-us-west-2.amazonaws.com/medhir-blog-dev/R%C3%A9sum%C3%A9+2019.pdf">
-            refer to my résumé
-          </a>{' '}
-          for further information on my previous work.
         </p>
       </section>
     );
