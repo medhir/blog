2a0b371f94163dca291365ba2999ad43f3bbe9b4
Medhir Bhargava <medhirbhargava@gmail.com>
2019-07-21 01:02:51 -0700

    Continue curve tool

--- client/src/Components/CurveTool/index.js
+++ client/src/Components/CurveTool/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RightUpArcedPeanoCurve } from './peano'
+import { PeanoCurves } from './peano'
 import './index.css'
 
 class CurveTool extends React.Component {
@@ -11,32 +11,17 @@ class CurveTool extends React.Component {
       <section className="curveTool">
         <h2>curve.tool</h2>
         <svg>
-          <RightUpArcedPeanoCurve size={200} />
-          {/* <path
-            stroke="black"
-            strokeWidth="5"
-            fill="transparent"
-            d="M 100 100 A 100 100 0 0 1 90"
-          />
-          <path
-            stroke="red"
-            strokeWidth="5"
-            fill="transparent"
-            d="M 75 75 A 25 25 0 1 0 100 50"
-          />
-          <path
-            stroke="black"
-            strokeWidth="5"
-            fill="transparent"
-            d="M 100 50 A 25 25 0 1 1 125 25"
-          />
-          <path
-            stroke="red"
-            strokeWidth="5"
-            fill="transparent"
-            d="M 125 25 A 25 25 0 0 0 150 50"
-          /> */}
+          <PeanoCurves.RightDown size={100} origin={{ x: 0, y: 75 }} />
+          <PeanoCurves.RightUp size={200} origin={{ x: 50, y: 75 }} />
+          <PeanoCurves.RightDown size={200} origin={{ x: 150, y: 75 }} />
+          <PeanoCurves.RightUp size={100} origin={{ x: 250, y: 75 }} />
+          <PeanoCurves.RightDown size={200} origin={{ x: 300, y: 75 }} />
+          <PeanoCurves.RightUp size={150} origin={{ x: 400, y: 75 }} />
         </svg>
+        <p>
+          open source space-filling curve generator for stretchable electrode
+          patterning.
+        </p>
       </section>
     )
   }
