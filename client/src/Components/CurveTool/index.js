451be4daf074c8efc645a79ed8e08d8f18ef177f
Medhir Bhargava <medhir@lightstep.com>
2019-07-31 21:47:15 -0700

    *sliders*

--- client/src/Components/CurveTool/index.js
+++ client/src/Components/CurveTool/index.js
@@ -5,7 +5,29 @@ import { SampleCurve } from './tile'
 import './index.css'
 
 class CurveTool extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      strokeWidth: 3,
+      radius: 10,
+    }
+    this.updateStrokeWidth.bind(this)
+    this.updateRadius.bind(this)
+  }
+
+  updateStrokeWidth(e) {
+    const updatedWidth = e.target.value
+    this.setState({ strokeWidth: updatedWidth })
+  }
+
+  updateRadius(e) {
+    const updatedRadius = e.target.value
+    this.setState({ radius: updatedRadius })
+  }
+
   render() {
+    const { strokeWidth, radius } = this.state
+
     return (
       <section className="curveTool">
         <h2>curve.tool</h2>
@@ -17,8 +39,32 @@ class CurveTool extends React.Component {
           Tile primitives that scale / connect seamlessly. Randomize orientation
           on a path to minimize strain on electrode wires.
         </p>
+        <label for="stroke-width">Stroke Width</label>
+        <input
+          type="range"
+          name="stroke-width"
+          min="0"
+          max="20"
+          value={strokeWidth}
+          id="stroke-width"
+          onChange={this.updateStrokeWidth.bind(this)}
+        />
+        <label for="radius">Radius</label>
+        <input
+          type="range"
+          name="radius"
+          min="0"
+          max="100"
+          value={radius}
+          id="radius"
+          onChange={this.updateRadius.bind(this)}
+        />
         <svg className="fullHeight">
-          <SampleCurve radius={20} strokeWidth={8} start={{ x: 50, y: 50 }} />
+          <SampleCurve
+            radius={radius}
+            strokeWidth={strokeWidth}
+            start={{ x: 50, y: 50 }}
+          />
         </svg>
       </section>
     )
