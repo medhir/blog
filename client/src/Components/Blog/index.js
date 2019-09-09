23ece867ae0b0acf171fb9db1fd6fe65735b5105
Medhir Bhargava <medhirbhargava@gmail.com>
2019-09-08 22:22:50 -0400

    Add support for absolute import paths (vscode + webpack config), delete old Loading.js file

--- client/src/Components/Blog/index.js
+++ client/src/Components/Blog/index.js
@@ -1,60 +1,85 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
 import { Route } from 'react-router-dom'
 import BlogList from './modules/BlogList'
-import Editor from '../Editor';
+import Editor from '../Editor'
 import api from './api'
-import './Blog.css';
+import './Blog.css'
 import md from './popplers'
-import Loading from '../../Layout/Loading';
+import Loading from 'Components/Loading'
 
 class Blog extends Component {
-    constructor (props) {
-        super(props)   
-        this.state = {
-            add: false,
-            posts: null, 
-            drafts: null
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      add: false,
+      posts: null,
+      drafts: null,
     }
+  }
 
-    componentDidMount = () => {
-        api.getPosts().then(success => {
-            const data = success.data
-            this.setState({
-                posts: data.posts, 
-                drafts: data.drafts
-            })
-        })
-    }
+  componentDidMount = () => {
+    api.getPosts().then(success => {
+      const data = success.data
+      this.setState({
+        posts: data.posts,
+        drafts: data.drafts,
+      })
+    })
+  }
 
-    // Re-call API on Blog Draft Save
-    handleUpdate = () => {
-        api.getPosts().then(success => {
-            const data = success.data
-            this.setState({
-                posts: data.posts, 
-                drafts: data.drafts
-            })
-        })
-    }
+  // Re-call API on Blog Draft Save
+  handleUpdate = () => {
+    api.getPosts().then(success => {
+      const data = success.data
+      this.setState({
+        posts: data.posts,
+        drafts: data.drafts,
+      })
+    })
+  }
 
-    render () {
-        if (this.state.posts && this.state.drafts) {
-            return (
-                <Fragment>
-                    <Route 
-                        exact path={ this.props.match.path } 
-                        component={ () => <BlogList match={ this.props.match } posts={ this.state.posts } drafts={ this.state.drafts }/> } />
-                    <Route exact path={ `${ this.props.match.path }/new/draft` } 
-                           component={ () => <Editor handleUpdate={ this.handleUpdate.bind(this) } markdown={ md } { ...this.props }/> }/>
-                    <Route path={ `${ this.props.match.path }/draft/:id` } 
-                           component={ () => <Editor handleUpdate={ this.handleUpdate.bind(this) } draft={ this.props.location.state } { ...this.props }/> }/>
-                </Fragment>
-            )
-        } else {
-            return <Loading />
-        }
+  render() {
+    if (this.state.posts && this.state.drafts) {
+      return (
+        <Fragment>
+          <Route
+            exact
+            path={this.props.match.path}
+            component={() => (
+              <BlogList
+                match={this.props.match}
+                posts={this.state.posts}
+                drafts={this.state.drafts}
+              />
+            )}
+          />
+          <Route
+            exact
+            path={`${this.props.match.path}/new/draft`}
+            component={() => (
+              <Editor
+                handleUpdate={this.handleUpdate.bind(this)}
+                markdown={md}
+                {...this.props}
+              />
+            )}
+          />
+          <Route
+            path={`${this.props.match.path}/draft/:id`}
+            component={() => (
+              <Editor
+                handleUpdate={this.handleUpdate.bind(this)}
+                draft={this.props.location.state}
+                {...this.props}
+              />
+            )}
+          />
+        </Fragment>
+      )
+    } else {
+      return <Loading />
     }
+  }
 }
 
-export default Blog;
+export default Blog
--- client/src/Components/Gallery/index.js
+++ client/src/Components/Gallery/index.js
@@ -1,109 +1,126 @@
-import React, { Component, Fragment } from 'react';
-import './Gallery.css';
-import Photo from './Photo';
-import Loading from '../../Layout/Loading';
-import api from './api';
+import React, { Component, Fragment } from 'react'
+import Loading from 'Components/Loading'
+import './Gallery.css'
+import Photo from './Photo'
+import api from './api'
 
 const BASE_PHOTO_URL = 'https://s3-us-west-2.amazonaws.com/medhir-blog-dev'
 
-const Album = (props) => {
-    return (
-        <div className="album" album={props.album} onClick={() => props.getPhotos(props.album)}>
-            <h1>{props.album}</h1>
-        </div>
-    );
-};
-
-const Albums = (props) => {
-    return (
-        <Fragment>
-            <section className="albums">
-                {
-                    props.albums.map((album) => <Album album={album} key={album} getPhotos={props.getPhotos} />)
-                }
-            </section>
-            <section className="addAlbum"></section>
-        </Fragment>
-    )
+const Album = props => {
+  return (
+    <div
+      className="album"
+      album={props.album}
+      onClick={() => props.getPhotos(props.album)}
+    >
+      <h1>{props.album}</h1>
+    </div>
+  )
 }
 
-const PhotoGallery = (props) => {
-    return (
-        <section className="photos">
-            {
-                props.photos.map((photo) => <Photo className="photo" src={`${BASE_PHOTO_URL}/${photo}`} key={photo} s3key={photo} />)
-            }
-        </section>
-    )
+const Albums = props => {
+  return (
+    <Fragment>
+      <section className="albums">
+        {props.albums.map(album => (
+          <Album album={album} key={album} getPhotos={props.getPhotos} />
+        ))}
+      </section>
+      <section className="addAlbum" />
+    </Fragment>
+  )
+}
+
+const PhotoGallery = props => {
+  return (
+    <section className="photos">
+      {props.photos.map(photo => (
+        <Photo
+          className="photo"
+          src={`${BASE_PHOTO_URL}/${photo}`}
+          key={photo}
+          s3key={photo}
+        />
+      ))}
+    </section>
+  )
 }
 
 // returns an array of Photo components
 // as a Gallery
 
 class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            albums: null,
-            album: null,
-            photos: [],
-            displayPhotos: [],
-            error: null
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      albums: null,
+      album: null,
+      photos: [],
+      displayPhotos: [],
+      error: null,
     }
+  }
 
-    getPhotos = (album) => {
-        api.getPhotos(album)
-            .then(response => {
-                this.setState({
-                    albums: null,
-                    album: album,
-                    photos: response.data
-                })
-            })
+  getPhotos = album => {
+    api.getPhotos(album).then(response => {
+      this.setState({
+        albums: null,
+        album: album,
+        photos: response.data,
+      })
+    })
+  }
+
+  addDisplayPhotos = () => {
+    const startIndex = this.state.displayPhotos.length
+    const newPhotoURLS = this.state.photos.slice(startIndex, startIndex + 1)
+    const newDisplayPhotos = this.state.displayPhotos.concat(newPhotoURLS)
+    this.setState({ displayPhotos: newDisplayPhotos })
+  }
+
+  componentDidMount = () => {
+    const AlbumName = 'main'
+    api
+      .getPhotos(AlbumName)
+      .then(response => {
+        this.setState({ album: AlbumName, photos: response.data })
+        const displayPhotos = this.state.photos.slice(0, 3)
+        this.setState({ displayPhotos: displayPhotos })
+      })
+      .catch(error => {
+        this.setState({ error: error })
+      })
+
+    window.addEventListener('scroll', this.onScroll, false)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, false)
+  }
+
+  onScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.scrollHeight - 900 &&
+      this.state.displayPhotos.length <= this.state.photos.length
+    ) {
+      this.addDisplayPhotos()
     }
+  }
 
-    addDisplayPhotos = () => {
-        const startIndex = this.state.displayPhotos.length
-        const newPhotoURLS = this.state.photos.slice(startIndex, startIndex + 1)
-        const newDisplayPhotos = this.state.displayPhotos.concat(newPhotoURLS)
-        this.setState({ displayPhotos: newDisplayPhotos })
+  render() {
+    if (this.state.albums) {
+      return <Albums albums={this.state.albums} getPhotos={this.getPhotos} />
+    } else if (this.state.album) {
+      return (
+        <PhotoGallery
+          album={this.state.album}
+          photos={this.state.displayPhotos}
+        />
+      )
+    } else {
+      return <Loading />
     }
-
-    componentDidMount = () => {
-        const AlbumName = 'main'
-        api.getPhotos(AlbumName)
-            .then((response) => {
-                this.setState({ album: AlbumName, photos: response.data })
-                const displayPhotos = this.state.photos.slice(0, 3)
-                this.setState({ displayPhotos: displayPhotos })
-            })
-            .catch((error) => {
-                this.setState({ error: error });
-            });
-
-        window.addEventListener('scroll', this.onScroll, false);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll, false);
-    }
-
-    onScroll = () => {
-        if ((window.innerHeight + window.scrollY) >= (document.body.scrollHeight - 900) && this.state.displayPhotos.length <= this.state.photos.length) {
-            this.addDisplayPhotos()
-        }
-    }
-
-    render() {
-        if (this.state.albums) {
-            return <Albums albums={this.state.albums} getPhotos={this.getPhotos} />;
-        } else if (this.state.album) {
-            return <PhotoGallery album={this.state.album} photos={this.state.displayPhotos} />;
-        } else {
-            return <Loading />;
-        }
-    }
+  }
 }
 
-export default Gallery;
+export default Gallery
