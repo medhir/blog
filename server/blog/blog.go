15446f62008cb7dd279ee6fdc5ae793d46835a78
Medhir Bhargava <mail@medhir.com>
2020-04-27 23:25:24 -0700

    Init blog component

---
+++ server/blog/blog.go
@@ -0,0 +1,71 @@
+package blog
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/medhir/blog/server/storage/gcs"
+)
+
+const bucket = "medhir-com"
+
+// Blog describes the methods available for the blog controller
+type Blog interface {
+	AddDraft(markdown, title string) error
+	SaveDraft(draft Draft) error
+	PublishPost(draft Draft) error
+	UpdatePost(post Post) error
+}
+
+type blog struct {
+	gcs gcs.GCS
+}
+
+// NewBlog returns a new blog controller
+func NewBlog(gcs gcs.GCS) Blog {
+	return &blog{
+		gcs: gcs,
+	}
+}
+
+// Post describes the data associated with a blog post
+type Post struct {
+	Title     string `json:"title"`
+	TitlePath string `json:"titlePath"`
+	Markdown  string `json:"markdown"`
+	Published int64  `json:"published"`
+	ID        string `json:"id"`
+}
+
+// Draft describes the data associated with a blog post draft
+type Draft struct {
+	ID       string `json:"id"`
+	Markdown string `json:"markdown"`
+	Saved    int64  `json:"saved"`
+	Title    string `json:"title"`
+}
+
+// AddDraft adds a draft
+func (b *blog) AddDraft(title string) error {
+	name := fmt.Sprintf("blog/drafts/%s.json", uuid)
+	draft := Draft{
+		ID:       uuid,
+		Markdown: markdown,
+		Saved:    makeTimestamp(),
+		Title:    title,
+	}
+	data, err := json.Marshal(draft)
+	if err != nil {
+		return fmt.Errorf("Unable to encode data to json - %v", err)
+	}
+	err = b.gcs.UploadObject(name, bucket)
+	if err != nil {
+		return fmt.Errorf("Unable to upload draft - %v", err)
+	}
+	return nil
+}
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
---
+++ server/blog/blog_test.go
@@ -0,0 +1,8 @@
+package blog
+
+import "testing"
+
+func TestAddDraft(t *testing.T) {
+	t.Run("Successfully uploads a blog post", func(t *testing.T) {})
+	t.Run("Returns an error if upload to gcs fails", func(t *testing.T) {})
+}
