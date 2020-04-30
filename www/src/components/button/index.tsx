131ee30f861fefe2349e3ab1f6c3f8c158431511
Medhir Bhargava <mail@medhir.com>
2020-04-29 21:25:58 -0700

    Add Button component

---
+++ www/src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from 'react'
+import styles from './button.module.scss'
+
+interface ButtonProps {
+  children: ReactNode
+  className?: string
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+}
+
+const Button = ({ children, className, onClick }: ButtonProps) => (
+  <button className={`${styles.Button} ${className}`} onClick={onClick}>
+    {children}
+  </button>
+)
+
+export default Button
+
+export const GreenButton = ({ children, onClick }: ButtonProps) => (
+  <Button className={styles.green} onClick={onClick}>
+    {children}
+  </Button>
+)
+
+export const RedButton = ({ children, onClick }: ButtonProps) => (
+  <Button className={styles.red} onClick={onClick}>
+    {children}
+  </Button>
+)
+
+export const MagentaButton = ({ children, onClick }: ButtonProps) => (
+  <Button className={styles.magenta} onClick={onClick}>
+    {children}
+  </Button>
+)
