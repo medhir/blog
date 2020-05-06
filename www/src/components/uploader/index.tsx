b1a6dc405c5aa368478053842d1397b10dab75a2
Medhir Bhargava <mail@medhir.com>
2020-05-06 15:32:13 -0700

    Add Uploader component & page (/photos/upload)

---
+++ www/src/components/uploader/index.tsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react'
+import Auth from '../auth'
+import styles from './uploader.module.scss'
+import http from '../../utility/http'
+
+interface FilesProps {
+  files: FileList
+}
+
+const Files = ({ files }: FilesProps) => {
+  if (files) {
+    const fileArray = Array.from(files)
+    return (
+      <div className="files">
+        <p>Photos to save:</p>
+        <ul>
+          {fileArray.map((file, i) => (
+            <li key={`file-${i}`}>{file.name}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  } else {
+    return null
+  }
+}
+
+const SuccessMessage = () => {
+  return (
+    <div className={styles.locations}>
+      <p>Photos saved!</p>
+    </div>
+  )
+}
+
+interface UploaderState {
+  progress: string
+  success: boolean
+  files: FileList
+  error: boolean
+}
+
+class Uploader extends Component<{}, UploaderState> {
+  constructor(props: Readonly<{}>) {
+    super(props)
+    this.state = {
+      progress: null,
+      success: null,
+      files: null,
+      error: null,
+    }
+  }
+
+  handleFileStateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      files: e.target.files,
+    })
+  }
+
+  handleProgressEvent = (e: ProgressEvent) => {
+    if (e.lengthComputable) {
+      const percentage = Math.round((e.loaded * 100) / e.total)
+      this.setState({
+        progress: `${percentage}%`,
+      })
+    }
+  }
+
+  handleUpload = (e) => {
+    e.preventDefault()
+    if (this.state.files) {
+      const formData = new FormData()
+      for (let i = 0; i < this.state.files.length; i++) {
+        formData.append('photo', this.state.files[i])
+      }
+      http
+        .Post('/photo', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          withCredentials: true,
+          onUploadProgress: this.handleProgressEvent,
+        })
+        .then(() => {
+          this.setState({
+            success: true,
+          })
+        })
+        .catch(() => {
+          this.setState({
+            error: true,
+          })
+        })
+    }
+  }
+
+  render() {
+    const { files, progress, success } = this.state
+    return (
+      <Auth prompt>
+        <form className={styles.imageForm}>
+          <input
+            type="file"
+            accept="image/*"
+            id="imagesInput"
+            className={styles.imagesInput}
+            multiple
+            onChange={this.handleFileStateChange}
+          />
+          <label htmlFor="imagesInput">Choose Images</label>
+          <button
+            className={styles.uploadButton}
+            onClick={(e) => {
+              this.handleUpload(e)
+            }}
+          >
+            Upload
+          </button>
+        </form>
+        <div className={styles.progressIndicator}>
+          <div style={{ width: progress }} />
+        </div>
+        {success && <SuccessMessage />}
+        {files && <Files files={files} />}
+      </Auth>
+    )
+  }
+}
+
+export default Uploader
