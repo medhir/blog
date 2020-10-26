2a494e918e77960d02b20781bb2bb850f4f4b708
Medhir Bhargava <mail@medhir.com>
2020-10-26 13:54:48 -0700

    Fix bug where extra line is added to token list on syntax highlighted blocks

--- www/src/components/mdx-viewer/codeBlock.tsx
+++ www/src/components/mdx-viewer/codeBlock.tsx
@@ -15,13 +15,15 @@ export default ({ children, className }) => {
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <code>
           <pre className={className} style={{ ...style, padding: '10px' }}>
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, i, a) =>
+              i < a.length - 1 ? ( // on syntax highlighting, for some reason there is an extra line registered as a token. this is to remove that last token from the list.
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ) : null
+            )}
           </pre>
         </code>
       )}
