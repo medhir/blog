748bea1280229f84271576aa13103f1e0799cf33
Medhir Bhargava <mail@medhir.com>
2024-05-30 18:15:39 -0700

    update photo url transformations

--- www/src/components/photos/index.tsx
+++ www/src/components/photos/index.tsx
@@ -47,7 +47,7 @@ const Photos = ({ auth, photos }: PhotosProps) => {
   //   };
 
     const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) => {
-        return `${src.split('/public')[0]}/w=${width},q=${quality}`;
+        return `${src.split('/public')[0]}/w=${width}`;
     };
 
         return (
@@ -66,7 +66,6 @@ const Photos = ({ auth, photos }: PhotosProps) => {
                   width: "100%",
                   height: "auto"
                 }}
-                quality={55}
                 placeholder={"blur"}
                 blurDataURL={photo.blurDataURL}
             />
