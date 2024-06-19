e077043a840b86821bfaf1a4ecb237d225e33ccb
Medhir Bhargava <mail@medhir.com>
2024-06-18 17:17:00 -0700

    Video playing on main page. Remove redirect to /photos

--- www/src/pages/photos/index.tsx
+++ www/src/pages/photos/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import http from "../../utility/http";
-import PhotosComponent, { PhotosProps } from "../../components/photos";
+import http from "@/utility/http";
+import PhotosComponent, { PhotosProps } from "@/components/photos";
 
 const Photos = ({ photos }: PhotosProps) => <PhotosComponent photos={photos} />;
 
--- www/src/pages/videos/index.tsx
+++ www/src/pages/videos/index.tsx
@@ -6,23 +6,48 @@ import Login from "@/components/auth/login";
 import MuxUploader from "@mux/mux-uploader-react";
 import http, { Protected } from "@/utility/http";
 import { CircularProgress } from "@material-ui/core";
+import { AlertData, ErrorAlert, SuccessAlert } from "@/components/alert";
+import { AxiosError } from "axios";
 
 interface VideoUploaderState {
+  id: string | null;
   endpoint: string | null;
+  successAlert: AlertData;
+  errorAlert: AlertData;
 }
 
 class VideoUploader extends Component<{}, VideoUploaderState> {
   constructor(props: Readonly<{}>) {
     super(props);
     this.state = {
+      id: null,
       endpoint: null,
+      successAlert: {
+        open: false,
+        message: "",
+      },
+      errorAlert: {
+        open: false,
+        message: "",
+      },
     };
+
+    this.getVideoURL = this.getVideoURL.bind(this);
+    this.postVideoObject = this.postVideoObject.bind(this);
+    this.closeSuccessAlert = this.closeSuccessAlert.bind(this);
+    this.closeErrorAlert = this.closeErrorAlert.bind(this);
   }
 
   componentDidMount() {
-    Protected.Client.Post("/video", {})
+    this.getVideoURL();
+  }
+
+  getVideoURL() {
+    Protected.Client.Get("/video")
       .then((success) => {
+        console.dir(success);
         this.setState({
+          id: success.data.id,
           endpoint: success.data.url,
         });
       })
@@ -31,12 +56,76 @@ class VideoUploader extends Component<{}, VideoUploaderState> {
       });
   }
 
+  postVideoObject() {
+    const { getVideoURL } = this;
+    const { id, endpoint } = this.state;
+    Protected.Client.Post("/video", {
+      id: id,
+      url: endpoint,
+    })
+      .then((success) => {
+        console.dir(success);
+        this.setState(
+          {
+            successAlert: {
+              open: true,
+              message: `video successfully saved`,
+            },
+            id: null,
+            endpoint: null,
+          },
+          getVideoURL
+        );
+      })
+      .catch((error: AxiosError) => {
+        this.setState({
+          errorAlert: {
+            open: true,
+            message: `unable to save video: ${error.response!!.data as string}`,
+          },
+        });
+      });
+  }
+
+  closeSuccessAlert() {
+    this.setState({
+      successAlert: {
+        open: false,
+        message: "",
+      },
+    });
+  }
+
+  closeErrorAlert() {
+    this.setState({
+      errorAlert: {
+        open: false,
+        message: "",
+      },
+    });
+  }
+
   render() {
-    const { endpoint } = this.state;
-    return endpoint ? (
-      <MuxUploader endpoint={endpoint} />
-    ) : (
-      <CircularProgress />
+    const { postVideoObject, closeSuccessAlert, closeErrorAlert } = this;
+    const { endpoint, errorAlert, successAlert } = this.state;
+    return (
+      <>
+        {endpoint ? (
+          <MuxUploader endpoint={endpoint} onSuccess={postVideoObject} />
+        ) : (
+          <CircularProgress />
+        )}
+        {errorAlert.open && (
+          <ErrorAlert open={errorAlert.open} onClose={closeErrorAlert}>
+            {errorAlert.message}
+          </ErrorAlert>
+        )}
+        {successAlert.open && (
+          <SuccessAlert open={successAlert.open} onClose={closeSuccessAlert}>
+            {successAlert.message}
+          </SuccessAlert>
+        )}
+      </>
     );
   }
 }
